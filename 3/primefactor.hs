4a49a0a8b3899e01395e50d66a53d1f5807e1969
cpireyre <cpireyre@student.42.fr>
2019-04-11 15:55:11 +0200

    Largest prime factor problem

---
+++ 3/primefactor.hs
@@ -0,0 +1,23 @@
+import System.Environment
+
+intSqRoot :: Int -> Int -> Int
+intSqRoot x y 
+    | (x * x >= y)  = x
+    | otherwise     = intSqRoot (x + 1) (y)
+
+findSqRt :: Int -> Int
+findSqRt x = intSqRoot 0 x
+
+listDivisors :: Int -> [Int]
+listDivisors x = [i | i <- [2 .. findSqRt x], x `mod` i == 0]
+
+isPrime :: Int -> Bool
+isPrime x
+    | (x < 2)                    = False
+    | (x == 2)                   = True
+    | (null divisors == True)    = True
+    | otherwise                  = False
+    where divisors = listDivisors x
+
+listPrimeFactors :: Int -> [Int] -- 600851475143 in the Euler problem statement.
+listPrimeFactors x = filter (isPrime) (listDivisors x)
